95605673b7112810c281b9c9002d8e2c3d06c4e1
raview69 <raviwicaksono@aol.com>
2022-03-06 22:50:18 +0700

    adding firebase module

--- src/components/page/Mainpage.js
+++ src/components/page/Mainpage.js
@@ -75,66 +75,75 @@ const Mainpage = () => {
 					zIndex: "-1",
 				}}
 			/>
+
 			<Container className="container">
 				<Row>
 					<Col>
 						<Row className="row-up">
 							<Navbar />
 						</Row>
-						<Row className="row-bottom">
-							<Col className="col-bottom">
-								<Row className="icon">
-									<Col>
-										<a href="https://www.instagram.com/ravi.wicaksono/">
-											<FaInstagram className="col-icon" />
-										</a>
-									</Col>
-									<Col>
-										<a href="https://twitter.com/RaviWicaksono2">
-											<FaTwitter className="col-icon" />
-										</a>
-									</Col>
-									<Col>
-										<a href="https://github.com/raview69">
-											<FaGithub className="col-icon" />
-										</a>
-									</Col>
-									<Col>
-										<a href="https://www.linkedin.com/in/muhamad-ravi-wicaksono-780637190/">
-											<FaLinkedin className="col-icon" />
-										</a>
-									</Col>
-									<Col>
-										<a href="#">
-											<FaFacebook className="col-icon" />
-										</a>
-									</Col>
-								</Row>
-								<h2>MUHAMAD RAVI WICAKSONO</h2>
-								<p className="typed">
-									I'm a&nbsp;
-									<Typed
-										strings={[
-											"web designer",
-											"full-stack developer",
-											"cat lover",
-										]}
-										typeSpeed={80}
-										backSpeed={40}
-										loop
-									/>
-								</p>
-							</Col>
-						</Row>
+
+						<Col
+							style={{
+								width: "100%",
+								height: height,
+							}}
+						>
+							<Row className="icon">
+								<Col>
+									<a href="https://www.instagram.com/ravi.wicaksono/">
+										<FaInstagram className="col-icon" />
+									</a>
+								</Col>
+								<Col>
+									<a href="https://twitter.com/RaviWicaksono2">
+										<FaTwitter className="col-icon" />
+									</a>
+								</Col>
+								<Col>
+									<a href="https://github.com/raview69">
+										<FaGithub className="col-icon" />
+									</a>
+								</Col>
+								<Col>
+									<a href="https://www.linkedin.com/in/muhamad-ravi-wicaksono-780637190/">
+										<FaLinkedin className="col-icon" />
+									</a>
+								</Col>
+								<Col>
+									<a href="#">
+										<FaFacebook className="col-icon" />
+									</a>
+								</Col>
+							</Row>
+							<Row className="row-bottom">
+								<Col className="col-bottom">
+									<h2>MUHAMAD RAVI WICAKSONO</h2>
+									<p className="typed">
+										I'm a&nbsp;
+										<Typed
+											strings={[
+												"web designer",
+												"full-stack developer",
+												"cat lover",
+											]}
+											typeSpeed={80}
+											backSpeed={40}
+											loop
+										/>
+									</p>
+								</Col>
+							</Row>
+							<iframe
+								src="https://my.spline.design/untitled-764d24f31dc3321c4417be9f939688e9/"
+								frameborder="0"
+								width="100%"
+								height="100%"
+							/>
+						</Col>
 					</Col>
 				</Row>
 			</Container>
-			<iframe
-				src="https://my.spline.design/untitled-764d24f31dc3321c4417be9f939688e9/"
-				frameborder="0"
-				width="100%"
-				height="100%"
-			/>
 		</div>
 	);
 };
