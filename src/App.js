729348f7e752c94f11d10ca9d672dfe8943d7520
raview69 <raviwicaksono@aol.com>
2022-03-27 02:31:44 +0700

    fix loader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainpage from "./components/page/Mainpage";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+	const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 8000);
+	}, []);
+
 	return (
 		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Mainpage />} />
-				</Routes>
-			</BrowserRouter>
+			{loading ? (
+				<div className="loader">
+					<RingLoader color={"#49AE5F"} loading={loading} size={50} />
+				</div>
+			) : (
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Mainpage />} />
+					</Routes>
+				</BrowserRouter>
+			)}
 		</>
 	);
 }
